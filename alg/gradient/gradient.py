900c816b12b111d281863e32829644a3ccaf52d2
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2023-01-09 18:40:47 +0200

    multiple feature cost and gradient

--- alg/gradient/gradient.py
+++ alg/gradient/gradient.py
@@ -1,5 +1,7 @@
+import copy
 import numpy as np
 from function.cost.mean_squared_error import compute_cost as mean_cost_f
+from function.cost.mean_squared_error import compute_cost_multiple as mean_cost_f_mul
 
 
 def compute_gradient(x, y, w, b):
@@ -38,6 +40,44 @@ def compute_descent(x, y, w, b, alpha, num_iters, cost_function, gradient_functi
     return w, b, j_history, p_history
 
 
+def compute_gradient_multiple(X, y, w, b):
+    m, n = X.shape
+
+    dj_dw = np.zeros((n,))
+    dj_db = 0
+
+    for i in range(m):
+        f_wb_i = np.dot(X[i], w) + b
+        err = f_wb_i - y[i]
+        for j in range(n):
+            dj_dw[j] = dj_dw[j] + err * X[i, j]
+
+        dj_db += err
+
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_dw, dj_db
+
+
+def compute_descent_multiple(X, y, w_in, b, alpha, num_iters, cost_function, gradient_function):
+    # cost history
+    j_history = []
+    w = copy.deepcopy(w_in)
+
+    for i in range(num_iters):
+        # Calculate gradient
+        dj_dw, dj_db = gradient_function(X, y, w, b)
+
+        # Update w and b params
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+
+        j_history.append(cost_function(X, y, w, b))
+
+    return w, b, j_history
+
+
 def run_linear():
     x = np.array([1.0, 2.0])
     y = np.array([300.0, 500.0])
@@ -54,5 +94,23 @@ def run_linear():
     assert round(b, 4) == 100.0116
 
 
+def run_multiple():
+    X = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+    y = np.array([460, 232, 178])
+
+    w = np.zeros((4,))
+    b = 0.
+    iterations = 1000
+    alpha = 5.0e-7
+
+    gradient_f = compute_gradient_multiple
+    w, b, history = compute_descent_multiple(X, y, w, b, alpha, iterations, mean_cost_f_mul, gradient_f)
+
+    w_rounded = [round(w_i, 3) for w_i in w]
+    assert w_rounded == [0.204, 0.004, -0.011, -0.066]
+    assert round(b, 3) == -0.002
+
+
 if __name__ == '__main__':
     run_linear()
+    run_multiple()
--- function/cost/mean_squared_error.py
+++ function/cost/mean_squared_error.py
@@ -23,6 +23,28 @@ def compute_cost(x, y, w, b):
     return total_cost
 
 
+def compute_cost_multiple(X, y, w, b):
+    """
+    Compute cost for multiple features
+    :param X: ndarray(m,n)
+    :param y: ndarray(m,)
+    :param w: ndarray(m,)
+    :param b: scalar
+    :return: total cost
+    """
+
+    m = X.shape[0]
+    sum_cost = 0
+    for i in range(m):
+        f_wb_i = np.dot(X[i], w) + b
+        cost = (f_wb_i - y[i]) ** 2
+        sum_cost += cost
+
+    total_cost = sum_cost / (2 * m)
+
+    return total_cost
+
+
 def run_linear():
     x = np.array([1.0, 2.0])
     y = np.array([300.0, 500.0])
@@ -31,5 +53,17 @@ def run_linear():
     assert cost == 0.0
 
 
+def run_linear_multiple():
+    X = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+    y = np.array([460, 232, 178])
+
+    w = np.array([0.39133535, 18.75376741, -53.36032453, -26.42131618])
+    b = 785.1811367994083
+
+    cost = compute_cost_multiple(X, y, w, b)
+    assert cost == 1.5578904428966628e-12
+
+
 if __name__ == '__main__':
     run_linear()
+    run_linear_multiple()
