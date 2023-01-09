5f4582a7f62a62dfcbb5f54ebfd43d7a00824c47
yaroslavambrozyak <ambrozyakyaroslav@gmail.com>
2023-01-09 14:41:49 +0200

    MSE function for 1D

---
+++ function/cost/README.md
@@ -0,0 +1,7 @@
+# Mean Squared Error (MSE)
+Measures the average of the squares of the errors that is, the average squared difference between the estimated values and the actual value
+## Formula
+![MSE](https://wikimedia.org/api/rest_v1/media/math/render/svg/e258221518869aa1c6561bb75b99476c4734108e)
+
+Yi - model prediction <br>
+Y_hat_i - actual value
---
+++ function/cost/mean_squared_error.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def compute_cost(x, y, w, b):
+    """
+    Computes cost function for 1D array values
+    :param x: ndarray (1,)
+    :param y: ndarray(1,)
+    :param w: scalar
+    :param b: scalar
+    :return: total cost
+    """
+    # get size of x array
+    n = x.shape[0]
+    sum_cost = 0
+    for i in range(n):
+        # get model prediction
+        f_wb = w * x[i] + b
+        cost = (f_wb - y[i]) ** 2
+        sum_cost += cost
+
+    total_cost = (1 / (2 * n)) * sum_cost
+    return total_cost
+
+
+def run_linear():
+    x = np.array([1.0, 2.0])
+    y = np.array([300.0, 500.0])
+
+    cost = compute_cost(x, y, 200, 100)
+    assert cost == 0.0
+
+
+if __name__ == '__main__':
+    run_linear()
